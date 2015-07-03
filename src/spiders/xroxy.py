2212d5a42f3fc3467398c2f9af38a0b2cd6553cb
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2015-07-03 17:03:35 -0300

    parse ip from html

--- src/spiders/xroxy.py
+++ src/spiders/xroxy.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from src.items import SrcItem
 
 
 class XroxySpider(scrapy.Spider):
     name = "xroxy"
     allowed_domains = ["xrory.com"]
     start_urls = (
-        'http://www.xroxy.com/',
+        'http://www.xroxy.com/proxylist.htm',
     )
 
     def parse(self, response):
-        pass
+        table = response.xpath('/html/body/div[1]/div[2]/table[1]')
+        self.log(table)
+        for tr in table:
+			item = SrcItem()
+			item['ip'] = tr.xpath('//table/tr/td[2]/a/text()').extract()
+			yield item
